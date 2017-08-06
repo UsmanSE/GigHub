bdcaefd8e393c511b70e57a67a9e974369524dea
Usman Arif <usmansoftdev@hotmail.com>
2017-08-06 05:04:04 +0500

    Create Add a Gig Form

--- GigHub/Models/ApplicationDbContext.cs
+++ GigHub/Models/ApplicationDbContext.cs
@@ -6,7 +6,7 @@ namespace GigHub.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public DbSet<Gig> Gigs { get; set; }
-        public Genre Genres { get; set; }
+        public DbSet<Genre> Genres { get; set; }
 
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
